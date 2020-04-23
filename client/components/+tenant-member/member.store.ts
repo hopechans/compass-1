96c84eb1ddeed3bf14e9940432af4d07568484cb
huangcheng <136970135@qq.com>
2020-04-23 13:55:23 +0800

    add tenant member page

---
+++ client/components/+tenant-member/member.store.ts
@@ -0,0 +1,46 @@
+import { observable,action, autorun,reaction} from "mobx";
+import { observer,disposeOnUnmount} from 'mobx-react'
+import { autobind } from "../../utils";
+import { TetantMember,tetantMemberApi } from "../../api/endpoints/tenant-member.api";
+import { ItemStore } from "../../item.store";
+
+
+@autobind()
+export class MemberStore extends ItemStore<TetantMember> {
+
+    @observable name:string
+    @observable dialogType:string
+ 
+    @action
+    changeItemName(name:string){
+      this.name = name
+    }
+
+    @action
+    changeDialogType(type:string){
+      this.dialogType = type
+    }
+
+    @action
+    clean(){
+      setTimeout(()=>{
+        this.name = ''
+        this.dialogType = 'add'
+      },1000)
+    }
+
+    loadAll() {
+      return this.loadItems(() => tetantMemberApi.list());
+    }
+
+    getByName(name: string, repo: string) {
+      return this.items.find(item => item.getName() === name);
+    }
+
+    async removeSelectedItems(){
+      console.log('---------delete',this.selectedItems)
+    }
+}
+
+
+export const memberStore = new MemberStore();
--- client/components/+tenant-member/member.tsx
+++ client/components/+tenant-member/member.tsx
@@ -1,25 +1,110 @@
 
-import './member.scss'
 import * as React from 'react'
 import { observer } from "mobx-react";
 import { Trans } from "@lingui/macro";
 import { RouteComponentProps } from "react-router";
 import { cssNames, stopPropagation } from "../../utils";
-import { getDetailsUrl } from "../../navigation";
+import { getDetailsUrl,getDetails } from "../../navigation";
 import { apiManager } from "../../api/api-manager";
-import { ItemListLayout } from "../item-object-list/item-list-layout";
+import { memberStore } from "./member.store";
+import { TetantMember } from "../../api/endpoints/tenant-member.api";
+import { computed } from "mobx";
+import { ItemListLayout, ItemListLayoutProps } from "../item-object-list/item-list-layout";
+import { navigation } from "../../navigation";
+import { tenantDepartmentURL } from '../+tenant';
+import { AddMemberDialog } from './member-dialog-add'
+import { MemberDeatil } from './member-detail'
+import { MembertMenu } from './member-menu'
+interface IMemberRouteProps{
+    id:string
+    name:string
+}
 
-interface Props{
-
+export interface DepartmentProps extends RouteComponentProps<IMemberRouteProps>{
+    store: TetantMember;
+}
+enum sortBy {
+    name = "name",
 }
 
 @observer
-export class Member extends React.Component<Props>{
+export class Member extends React.Component<DepartmentProps>{
 
-    render(){
-        return(
-            <div>todo...</div>
-        )
+    constructor(props:any){
+        super(props)
     }
 
+    componentDidMount() {
+    }
+
+    get selectedRole() {
+        const paramsDetail = getDetails()
+        return memberStore.items.find(member => {
+            return member.getId() == paramsDetail ;
+         });
+    }
+
+    hideDetails = () => {
+        this.showDetails(null);
+    }
+
+    showDetails = (item: TetantMember) => {
+        if (!item) {
+            navigation.searchParams.merge({
+                details:null
+            })
+        }
+        else {
+          navigation.searchParams.merge({
+              details: item.getId(),
+          })
+        }
+    }
+    render(){
+        return(
+            <>
+               <ItemListLayout
+                    className="tetantMember"
+                    store={memberStore}
+                    isClusterScoped={true}
+                    isSelectable={true}
+                    sortingCallbacks={{
+                        [sortBy.name]: (item: TetantMember) => item.getName(),
+                    }}
+                    searchFilters={[
+                        (item: TetantMember) => item.getName(),
+                        (item: TetantMember) => item.getId(),
+                   
+                    ]}
+                    renderHeaderTitle={<Trans>Member Manager</Trans>}
+                    renderTableHeader={[
+                        { title: <Trans>ID</Trans>, className: "id" },
+                        { title: <Trans>name</Trans>, className: "name", sortBy:sortBy.name},
+
+                    ]}
+                    renderTableContents={(item: TetantMember) => [
+                        item.getId(),
+                        item.getName(),
+                    ]}
+                    renderItemMenu={(item: TetantMember) => {
+                        return <MembertMenu object={item}/>
+                    }}
+                    addRemoveButtons={{
+                        onAdd: () => AddMemberDialog.open(),
+                        addTooltip: <Trans>Create new department</Trans>
+                    }}
+                    detailsItem={this.selectedRole}
+                    onDetails={this.showDetails}
+                />
+                <AddMemberDialog/>
+                <MemberDeatil
+                    selectItem={this.selectedRole}
+                    hideDetails={this.hideDetails}
+                >
+                </MemberDeatil>
+            </>
+        )
+    }
 }
+
+
