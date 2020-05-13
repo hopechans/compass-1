366274b300def932d06640ca300ed53b8ff41d10
pskishere <pskishere@gmail.com>
2020-05-13 18:39:43 +0800

    Add FormRender configuration generator

--- client/components/+deploymentengine-graph/graph.tsx
+++ client/components/+deploymentengine-graph/graph.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import {FRender} from "../ali-formrender";
 import {FormRenderStore} from "../ali-formrender/ali-formrender.store";
-import {autorun} from "mobx";
+import {autorun, keys} from "mobx";
 
+interface LooseObject {
+    [key: string] : any
+}
 
 interface IGraphProps {
 }
@@ -14,9 +17,22 @@ export class Graph extends React.Component<IGraphProps> {
     // private formRenderStore: any
 
     state = {formData: {}, valid: [] as string[]}
+    nestedList: any;
+    keyWordMap: any;
 
     constructor(props: IGraphProps) {
         super(props);
+        this.nestedList = [];
+        this.keyWordMap = {
+            "string": ["title", "required", "description", "message", "type",
+                "pattern"],
+            "select": ["title", "required", "description", "message", "type",
+                "select"],
+            "array": ["title", "required", "description", "message", "type",
+                "array"],
+            "number": ["title", "required", "description", "message", "type"],
+            "boolean": ["title", "required", "description", "message", "type"],
+        };
 
         // autorun(() => {
         //     this.formRenderStore = new FormRenderStore(
@@ -24,8 +40,15 @@ export class Graph extends React.Component<IGraphProps> {
         // })
     }
 
-    include(l:any, a:any) {
-        return !l.include(a)
+    setNestedList(formData: any) {
+        this.nestedList = [];
+        if (formData.hasOwnProperty("properties")) {
+            for (let i = 0; i < formData.properties.length; i++) {
+                if (formData.properties[i].nested && formData.properties[i].id != "") {
+                    this.nestedList.push(formData.properties[i].id);
+                }
+            }
+        }
     }
 
     propsSchema = {
@@ -37,11 +60,31 @@ export class Graph extends React.Component<IGraphProps> {
                 items: {
                     type: "object",
                     properties: {
+                        // id
+                        id: {
+                            title: "选项id",
+                            type: "string",
+                            "ui:width": "50%",
+                        },
                         // title
                         title: {
                             title: "名称(title)",
                             type: "string",
                             "ui:width": "50%",
+                        },
+                        // nested
+                        nested: {
+                            title: "设置为嵌套模版(nested)",
+                            type: "boolean",
+                            "ui:width": "50%",
+                            "ui:widget": "switch",
+                        },
+                        // required
+                        required: {
+                            title: "设置为必填项(required)",
+                            type: "boolean",
+                            "ui:width": "50%",
+                            "ui:widget": "switch",
                         },
                         // description
                         description: {
@@ -55,12 +98,6 @@ export class Graph extends React.Component<IGraphProps> {
                             type: "string",
                             "ui:width": "50%",
                         },
-                        // required
-                        required: {
-                            title: "设置为必填项(required)",
-                            type: "boolean",
-                            "ui:widget": "switch",
-                        },
                         type: {
                             title: "类型(type)",
                             type: "string",
@@ -69,7 +106,6 @@ export class Graph extends React.Component<IGraphProps> {
                                 "boolean",
                                 "select",
                                 "number",
-                                "range",
                                 "array",
                             ],
                             enumNames: [
@@ -77,8 +113,7 @@ export class Graph extends React.Component<IGraphProps> {
                                 "布尔值(boolean)",
                                 "选择框(select)",
                                 "数字(number)",
-                                "范围(range)",
-                                "数组多选(array)",
+                                "嵌套(array)",
                             ],
                         },
                         // string
@@ -88,42 +123,42 @@ export class Graph extends React.Component<IGraphProps> {
                             "ui:width": "50%",
                             "ui:hidden": "{{ rootValue.type != 'string'}}"
                         },
-                        format: {
-                            title: "组件格式(format)",
-                            type: "string",
-                            "ui:width": "50%",
-                            enum: [
-                                "",
-                                "time",
-                                "date",
-                                "dateTime",
-                            ],
-                            enumNames: [
-                                "",
-                                "time",
-                                "date",
-                                "dateTime",
-                            ],
-                            "ui:hidden": "{{ !['string', 'array'].includes(rootValue.type) }}"
-                        },
+                        // format: {
+                        //     title: "组件格式(format)",
+                        //     type: "string",
+                        //     "ui:width": "50%",
+                        //     enum: [
+                        //         "",
+                        //         "time",
+                        //         "date",
+                        //         "dateTime",
+                        //     ],
+                        //     enumNames: [
+                        //         "",
+                        //         "time",
+                        //         "date",
+                        //         "dateTime",
+                        //     ],
+                        //     "ui:hidden": "{{ !['string'].includes(rootValue.type) }}"
+                        // },
                         default: {
                             title: "默认值(default)",
                             type: "string",
                             "ui:width": "50%",
                             "ui:hidden": "{{ rootValue.type != 'string'}}"
                         },
-                        minLength: {
-                            title: "最小长度(minLength)",
-                            type: "number",
-                            "ui:width": "50%",
-                            "ui:hidden": "{{ rootValue.type !== 'string' }}"
-                        },
-                        maxLength: {
-                            title: "最大长度(maxLength)",
-                            type: "number",
-                            "ui:width": "50%",
-                            "ui:hidden": "{{ rootValue.type !== 'string' }}"
-                        },
+                        // minLength: {
+                        //     title: "最小长度(minLength)",
+                        //     type: "number",
+                        //     "ui:width": "50%",
+                        //     "ui:hidden": "{{ rootValue.type !== 'string' }}"
+                        // },
+                        // maxLength: {
+                        //     title: "最大长度(maxLength)",
+                        //     type: "number",
+                        //     "ui:width": "50%",
+                        //     "ui:hidden": "{{ rootValue.type !== 'string' }}"
+                        // },
                         // select
                         select: {
                             title: "键值对(select)",
@@ -141,67 +176,88 @@ export class Graph extends React.Component<IGraphProps> {
                                     }
                                 }
                             },
-                            "ui:hidden": "{{ !['select', 'array'].includes(rootValue.type) }}"
+                            "ui:hidden": "{{ !['select'].includes(rootValue.type) }}"
                         },
                         // number
-                        // uiConfig
-                        uiConfig: {
-                            title: "ui样式(uiConfig)",
-                            type: "boolean",
-                            "ui:widget": "switch"
+                        // nested
+                        array: {
+                            title: "嵌套模版id(array)",
+                            type: "array",
+                            items: {
+                                type: "object",
+                                properties: {
+                                    template: {
+                                        title: "-",
+                                        type: "string",
+                                        enum: () => {
+                                            return this.nestedList
+                                        },
+                                        enumNames: () => {
+                                            return this.nestedList
+                                        }
+                                    }
+                                }
+                            },
+                            "ui:hidden": "{{ !['array'].includes(rootValue.type) }}"
                         },
-                        "ui:className": {
-                            title: "ui类名(ui:className)",
-                            type: "string",
-                            "ui:width": "50%",
-                            "ui:hidden": "{{ rootValue.uiConfig === false}}"
-                        },
-                        "ui:width": {
-                            title: "ui宽度(ui:width)",
-                            type: "number",
-                            "ui:width": "50%",
-                            "ui:hidden": "{{ rootValue.uiConfig === false}}"
-                        },
-                        "ui:widget": {
-                            title: "ui插件(ui:widget)",
-                            type: "string",
-                            enum: [
-                                "",
-                                "color",
-                                "radio",
-                                "upload",
-                                "textarea",
-                                "date",
-                            ],
-                            enumNames: [
-                                "",
-                                "color",
-                                "radio",
-                                "upload",
-                                "textarea",
-                                "date",
-                            ],
-                            "ui:width": "50%",
-                            "ui:hidden": "{{ rootValue.uiConfig === false }}"
-                        },
-                        // ui:options
-                        uiOptions: {
-                            title: "ui配置(uiOptions)",
-                            type: "boolean",
-                            "ui:widget": "switch"
-                        },
-                        "prefix": {
-                            title: "前缀(prefix)",
-                            type: "string",
-                            "ui:width": "50%",
-                            "ui:hidden": "{{ rootValue.uiOptions === false }}"
-                        },
-                        "suffix": {
-                            title: "后缀(suffix)",
-                            type: "string",
-                            "ui:width": "50%",
-                            "ui:hidden": "{{ rootValue.uiOptions === false }}"
-                        },
+                        // // uiConfig
+                        // uiConfig: {
+                        //     title: "ui样式(uiConfig)",
+                        //     type: "boolean",
+                        //     "ui:widget": "switch"
+                        // },
+                        // "ui:className": {
+                        //     title: "ui类名(ui:className)",
+                        //     type: "string",
+                        //     "ui:width": "50%",
+                        //     "ui:hidden": "{{ rootValue.uiConfig === false}}"
+                        // },
+                        // "ui:width": {
+                        //     title: "ui宽度(ui:width)",
+                        //     type: "number",
+                        //     "ui:width": "50%",
+                        //     "ui:hidden": "{{ rootValue.uiConfig === false}}"
+                        // },
+                        // "ui:widget": {
+                        //     title: "ui插件(ui:widget)",
+                        //     type: "string",
+                        //     enum: [
+                        //         "",
+                        //         "color",
+                        //         "radio",
+                        //         "upload",
+                        //         "textarea",
+                        //         "date",
+                        //     ],
+                        //     enumNames: [
+                        //         "",
+                        //         "color",
+                        //         "radio",
+                        //         "upload",
+                        //         "textarea",
+                        //         "date",
+                        //     ],
+                        //     "ui:width": "50%",
+                        //     "ui:hidden": "{{ rootValue.uiConfig === false }}"
+                        // },
+                        // // ui:options
+                        // uiOptions: {
+                        //     title: "ui配置(uiOptions)",
+                        //     type: "boolean",
+                        //     "ui:widget": "switch"
+                        // },
+                        // "prefix": {
+                        //     title: "前缀(prefix)",
+                        //     type: "string",
+                        //     "ui:width": "50%",
+                        //     "ui:hidden": "{{ rootValue.uiOptions === false }}"
+                        // },
+                        // "suffix": {
+                        //     title: "后缀(suffix)",
+                        //     type: "string",
+                        //     "ui:width": "50%",
+                        //     "ui:hidden": "{{ rootValue.uiOptions === false }}"
+                        // },
                     }
                 },
                 "ui:options": {
@@ -221,21 +277,66 @@ export class Graph extends React.Component<IGraphProps> {
         this.setState({formData: value})
     }
 
+    adorn(formData: any) {
+        if (formData.hasOwnProperty("properties")) {
+            const properties: LooseObject = {};
+
+            for (let i = 0; i < formData.properties.length; i++) {
+
+                const props = formData.properties[i];
+                const propsId = props.id;
+                const propsType = props.type;
+                const propsKeys = Object.keys(props);
+                const args: LooseObject = {};
+
+                // without nested
+                for (let i=0; i < propsKeys.length; i++) {
+                    const key = propsKeys[i]
+                    if (this.keyWordMap[propsType].includes(key) && props[key] != "") {
+
+                        if (propsType == "select" && key == "select") {
+                            const enumArray = [];
+                            const enumNamesArray = [];
+                            // Enum EnumNames
+                            for (let i=0; i < props[key].length; i++) {
+                                enumArray.push(props[key][i]["key"])
+                                enumNamesArray.push(props[key][i]["value"])
+                            }
+                            args["enum"] = enumArray;
+                            args["enumNames"] = enumNamesArray;
+                        }
+                        else{
+                            args[key] = props[key]
+                        }
+                    }
+                }
+                // with nested
+
+
+                // set properties
+                properties[propsId] = args;
+
+            }
+            return {type: "object", properties: properties}
+        }
+    }
+
     onSubmit = () => {
         if (this.state.valid.length > 0) {
             alert(`校验未通过字段：${this.state.valid.toString()}`);
         } else {
+            console.log(this.adorn(this.state.formData))
             const thisFormData = JSON.stringify(this.state.formData, null, 2)
             alert(thisFormData);
             console.log(thisFormData)
-
         }
     }
 
     render() {
-        const bgStyle={
-            backgroundColor:'#fff'
+        const bgStyle = {
+            backgroundColor: '#fff'
         }
+        this.setNestedList(this.state.formData)
         return (
             <div style={bgStyle}>
                 <FRender
