ec09652cdfcef3af20d33cef88d702a858fb97d7
laik <laik.lj@me.com>
2020-06-11 05:02:52 -0400

    Fix typo in ovn

--- client/components/+ovn/ovn.tsx
+++ client/components/+ovn/ovn.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react'
+import * as React from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { Redirect, Route, Switch } from "react-router";
 import { MainLayout, TabRoute } from "../layout/main-layout";
 import { Trans } from "@lingui/macro";
 import { namespaceStore } from "../+namespaces/namespace.store";
-import { ovnURL,ovnVlanRoute} from './ovn.route'
-import { Vlan} from '../+ove-vlan'
-interface Props extends RouteComponentProps { }
+import { ovnURL, ovnVlanRoute } from "./ovn.route";
+import { Vlan } from "../+ovn-vlan";
+interface Props extends RouteComponentProps {}
 
 export class Ovn extends React.Component<Props> {
   static get tabRoutes(): TabRoute[] {
@@ -16,19 +16,21 @@ export class Ovn extends React.Component<Props> {
         title: <Trans>Vlan</Trans>,
         component: Vlan,
         url: ovnURL({ query }),
-        path: ovnVlanRoute.path
+        path: ovnVlanRoute.path,
       },
-    ]
-  };
+    ];
+  }
   render() {
     const tabRoutes = Ovn.tabRoutes;
     return (
       <MainLayout>
         <Switch>
-          {tabRoutes.map((route, index) => <Route key={index} {...route} />)}
+          {tabRoutes.map((route, index) => (
+            <Route key={index} {...route} />
+          ))}
           <Redirect to={tabRoutes[0].url} />
         </Switch>
       </MainLayout>
-    )
+    );
   }
-} 
+}
