552ce48283c7cfdbb376b8f73e54c00ece921f37
pskishere <pskishere@gmail.com>
2020-06-17 17:42:07 +0800

    add workspace results script volume mounts

--- client/components/+tekton-task-detail/task-step-details.tsx
+++ client/components/+tekton-task-detail/task-step-details.tsx
@@ -8,6 +8,10 @@ import {Divider} from "@material-ui/core";
 import {EnvironmentDetails} from "../+deploy-container/env-details";
 import {TaskStep, taskStep} from "./common";
 import {SubTitle} from "../layout/sub-title";
+import {WorkspacesDetails} from "./workspaces-details";
+import {WorkDirDetails} from "./workdir-details";
+import {ResultsDetails} from "./results-details";
+import {ScriptDetails} from "./script-details";
 
 interface Props<T = any> extends Partial<Props> {
   value?: T;
@@ -43,6 +47,10 @@ export class TaskStepDetails extends React.Component<Props> {
           onChange={value => (this.value.image = value)}
         />
         <br/>
+        <WorkDirDetails
+          value={this.value.workingDir}
+          onChange={value => this.value.workingDir = value}/>
+        <br/>
         <ArgsDetails
           value={this.value.args}
           onChange={value => this.value.args = value}
@@ -60,6 +68,18 @@ export class TaskStepDetails extends React.Component<Props> {
           onChange={value => this.value.environment = value}
         />
         <Divider/>
+        <br/>
+        <WorkspacesDetails
+          value={this.value.workspaces}
+          onChange={value => this.value.workspaces = value}/>
+        <Divider/>
+        <br/>
+        <ResultsDetails
+          value={this.value.results}
+          onChange={value => this.value.results = value}/>
+        <br/>
+        <Divider/>
+        <ScriptDetails />
       </>
     )
   }
