121696d629a103fdba3ac3ceb39d557899810632
pskishere <pskishere@gmail.com>
2020-06-19 20:57:09 +0800

    Fix task component callback

--- client/components/+tekton-pipeline/copy-task-dialog.tsx
+++ client/components/+tekton-pipeline/copy-task-dialog.tsx
@@ -1,4 +1,4 @@
-import { observer } from "mobx-react";
+import {observer} from "mobx-react";
 import React from "react";
 import {
   PipelineParamsDetails,
@@ -6,11 +6,11 @@ import {
   TaskNameDetails,
   MultiTaskStepDetails, PipelineParams, PipelineResources, TaskStep, taskStep
 } from "../+tekton-task-detail";
-import { observable } from "mobx";
-import { Dialog } from "../dialog";
-import { Wizard, WizardStep } from "../wizard";
-import { Trans } from "@lingui/macro";
-import { ActionMeta } from "react-select/src/types";
+import {observable} from "mobx";
+import {Dialog} from "../dialog";
+import {Wizard, WizardStep} from "../wizard";
+import {Trans} from "@lingui/macro";
+import {ActionMeta} from "react-select/src/types";
 import {SubTitle} from "../layout/sub-title";
 import {Input} from "../input";
 import {_i18n} from "../../i18n";
@@ -18,7 +18,9 @@ import {Task} from "../../api/endpoints";
 
 interface Props<T = any> extends Partial<Props> {
   value?: T;
+
   onChange?(option: T, meta?: ActionMeta): void;
+
   themeName?: "dark" | "light" | "outlined";
 }
 
@@ -28,7 +30,6 @@ export interface TaskResult {
   pipelineResources: PipelineResources[],
   taskSteps: TaskStep[],
 }
-
 
 export const task: TaskResult = {
   taskName: "defaultName",
@@ -41,10 +42,6 @@ export const task: TaskResult = {
 export class CopyTaskDialog extends React.Component<Props> {
   @observable value: TaskResult = this.props.value || task;
   @observable static isOpen = false;
-  @observable taskName: string = "";
-  @observable pipelineParams: PipelineParams[] = [];
-  @observable pipelineResources: PipelineResources[] = [];
-  @observable taskSteps: TaskStep[] = [taskStep];
 
   static open() {
     CopyTaskDialog.isOpen = true;
@@ -59,6 +56,7 @@ export class CopyTaskDialog extends React.Component<Props> {
   }
 
   handle = async () => {
+    console.log(this.value)
   }
 
   render() {
@@ -71,25 +69,25 @@ export class CopyTaskDialog extends React.Component<Props> {
       >
         <Wizard className="CopyAddDeployDialog" header={header} done={this.close}>
           <WizardStep contentClass="flex gaps column" next={this.handle}>
-            <SubTitle title={"Task Name"} />
+            <SubTitle title={"Task Name"}/>
             <Input
               required={true}
               placeholder={_i18n._("Task Name")}
               value={this.value.taskName}
               onChange={value => this.value.taskName = value}
             />
-            <br />
-            <PipelineParamsDetails value={this.pipelineParams} onChange={value => {
-              this.pipelineParams = value
-            }} />
-            <br />
+            <br/>
+            <PipelineParamsDetails value={this.value.pipelineParams} onChange={value => {
+              this.value.pipelineParams = value
+            }}/>
+            <br/>
             <PipelineResourceDetails value={this.value.pipelineResources} onChange={value => {
               this.value.pipelineResources = value
-            }} />
-            <br />
+            }}/>
+            <br/>
             <MultiTaskStepDetails value={this.value.taskSteps} onChange={value => {
               this.value.taskSteps = value
-            }} />
+            }}/>
           </WizardStep>
         </Wizard>
       </Dialog>
