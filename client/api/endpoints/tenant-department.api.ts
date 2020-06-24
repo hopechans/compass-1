f2206a244495b2651d9dc3f0c5e29c10b4448b1a
pskishere <pskishere@gmail.com>
2020-06-24 13:47:55 +0800

    add department git register

--- client/api/endpoints/tenant-department.api.ts
+++ client/api/endpoints/tenant-department.api.ts
@@ -4,20 +4,32 @@ import {autobind} from "../../utils";
 import {apiTenant} from "../index"
 import {Namespace} from "./namespaces.api";
 
+export interface Stack {
+  address: string
+  verification?: string
+  token?: string
+  user?: string
+  password?: string
+}
+
+export interface TenantDepartmentSpec {
+  namespace: Namespace[];
+  defaultNamespace: string;
+  gits?: Stack[];
+  registers?: Stack[];
+}
+
 @autobind()
 export class TenantDepartment extends KubeObject {
-    static kind = "BaseDepartment";
+  static kind = "BaseDepartment";
 
-    spec: {
-        namespace: Namespace[];
-        defaultNamespace: string;
-    }
+  spec: TenantDepartmentSpec
 }
 
 export const tenantDepartmentApi = new KubeApi({
-    kind: TenantDepartment.kind,
-    apiBase: "/apis/fuxi.nip.io/v1/basedepartments",
-    isNamespaced: true,
-    objectConstructor: TenantDepartment,
-    request: apiTenant
+  kind: TenantDepartment.kind,
+  apiBase: "/apis/fuxi.nip.io/v1/basedepartments",
+  isNamespaced: true,
+  objectConstructor: TenantDepartment,
+  request: apiTenant
 });
