3b3cda38d17a70ec0255be892d8b41e260fd089c
withlin <withlin@apache.org>
2020-07-12 20:11:31 +0800

    refactor pipeline-run code

--- client/components/+tekton-pipelinerun/pipelinerun.store.ts
+++ client/components/+tekton-pipelinerun/pipelinerun.store.ts
@@ -10,22 +10,20 @@ import { initData } from "../+tekton-graph/graphs";
 export class PipelineRunStore extends KubeObjectStore<PipelineRun> {
   api = pipelineRunApi;
 
-
   getNodeData(pipelineRun: PipelineRun) {
-    let graphName: string = ""
+    let graphName: string = "";
     pipelineRun.getAnnotations().filter((item) => {
       const R = item.split("=");
       if (R[0] == "fuxi.nip.io/tektongraphs") {
-        graphName = R[1]
+        graphName = R[1];
       }
     });
-    console.log(graphName)
+
     if (graphName) {
       return JSON.parse(tektonGraphStore.getByName(graphName).spec.data);
     }
     return initData;
   }
-  
 }
 
 export const pipelineRunStore = new PipelineRunStore();
