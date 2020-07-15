9a1eb56ceb654fac8104e6d32c68964e3b3048a0
pskishere <pskishere@gmail.com>
2020-07-15 17:49:26 +0800

    add graph changeSize

--- client/components/+tekton-pipeline/pipeline.store.ts
+++ client/components/+tekton-pipeline/pipeline.store.ts
@@ -1,9 +1,9 @@
-import { autobind } from "../../utils";
-import { KubeObjectStore } from "../../kube-object.store";
-import { pipelineApi, Pipeline, tektonGraphApi } from "../../api/endpoints";
-import { apiManager } from "../../api/api-manager";
-import { tektonGraphStore } from "../+tekton-graph/tekton-graph.store";
-import { initData } from "../+tekton-graph/graphs";
+import {autobind} from "../../utils";
+import {KubeObjectStore} from "../../kube-object.store";
+import {pipelineApi, Pipeline, tektonGraphApi, PipelineRun} from "../../api/endpoints";
+import {apiManager} from "../../api/api-manager";
+import {tektonGraphStore} from "../+tekton-graph/tekton-graph.store";
+import {initData} from "../+tekton-graph/graphs";
 
 @autobind()
 export class PipelineStore extends KubeObjectStore<Pipeline> {
@@ -21,6 +21,22 @@ export class PipelineStore extends KubeObjectStore<Pipeline> {
       return JSON.parse(tektonGraphStore.getDataByName(graphName));
     }
     return initData;
+  }
+
+  getNodeSize(pipeline: Pipeline) {
+    let graphName: string = "";
+    pipeline.getAnnotations().filter((item) => {
+      const R = item.split("=");
+      if (R[0] == "fuxi.nip.io/tektongraphs") {
+        graphName = R[1];
+      }
+    });
+
+    if (graphName) {
+      const tektonGraph = tektonGraphStore.getByName(graphName)
+      return {width: tektonGraph.spec.width, height: tektonGraph.spec.height}
+    }
+    return null;
   }
 }
 
