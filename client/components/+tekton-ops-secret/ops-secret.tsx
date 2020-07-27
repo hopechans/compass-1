4dda42c75316ef3ebc9e85a5efbdd92e4693456f
pskishere <pskishere@gmail.com>
2020-07-27 18:53:37 +0800

    update plugin

--- client/components/+tekton-ops-secret/ops-secret.tsx
+++ client/components/+tekton-ops-secret/ops-secret.tsx
@@ -1,8 +1,6 @@
-import {observer} from "mobx-react";
 import {Secrets} from "../+config-secrets";
 import { observable } from "mobx";
 
-@observer
 export class OpsSecrets extends Secrets  {
   @observable className = "OpsSecrets"
 }
