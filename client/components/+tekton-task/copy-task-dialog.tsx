a26c2d2309f21b4d0975cd696b6533c5bd732b44
withlin <withlin@apache.org>
2020-08-06 12:10:55 +0800

    support status animate

--- client/components/+tekton-task/copy-task-dialog.tsx
+++ client/components/+tekton-task/copy-task-dialog.tsx
@@ -104,6 +104,41 @@ export class CopyTaskDialog extends React.Component<Props> {
     this.node = node;
   }
 
+  /**
+   * 计算字符串的长度
+   * @param {string} str 指定的字符串
+   */
+  calcStrLen = (str: string) => {
+    var len = 0;
+    for (var i = 0; i < str.length; i++) {
+      if (str.charCodeAt(i) > 0 && str.charCodeAt(i) < 128) {
+        len++;
+      } else {
+        len += 2;
+      }
+    }
+    return len;
+  };
+
+  /**
+   * 计算显示的字符串
+   * @param {string} str 要裁剪的字符串
+   * @param {number} maxWidth 最大宽度
+   * @param {number} fontSize 字体大小
+   */
+  fittingString = (str: string, maxWidth: number, fontSize: number) => {
+    var fontWidth = fontSize * 1.3; //字号+边距
+    maxWidth = maxWidth * 2; // 需要根据自己项目调整
+    var width = this.calcStrLen(str) * fontWidth;
+    var ellipsis = "…";
+    if (width > maxWidth) {
+      var actualLen = Math.floor((maxWidth - 10) / fontWidth);
+      var result = str.substring(0, actualLen) + ellipsis;
+      return result;
+    }
+    return str;
+  };
+
   loadData = async (name: string, namespace: string) => {
     try {
       const task = taskStore.getByName(name, namespace);
